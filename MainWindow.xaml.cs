ff92ded8dbca44978ac1bb0d0b2fa9b447ae1f1d
Jakub Cywka <jakub.ryszard.cywka@gmail.com>
2024-05-20 18:21:35 +0200

    Add button

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace TodoLista
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
     }
 }
